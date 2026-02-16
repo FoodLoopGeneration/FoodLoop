acf9e7a1d806d4dedea372eb486b37b73dabcda1
alice <mazzolaa518@gmail.com>
2026-02-16 12:22:46 +0100

    First commit

---
+++ src/main/java/com/generation/foodloop/FoodloopApplication.java
@@ -0,0 +1,13 @@
+package com.generation.foodloop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodloopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodloopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/generation/foodloop/FoodloopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generation.foodloop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodloopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
