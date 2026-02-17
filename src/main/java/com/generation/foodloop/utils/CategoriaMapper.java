2fdec284421c9eb75b33b44208a3541f50a11a40
Gennaro <171047036+GenMoz22@users.noreply.github.com>
2026-02-17 11:34:21 +0100

    Aggiunti Mapper

--- src/main/java/com/generation/foodloop/utils/CategoriaMapper.java
+++ src/main/java/com/generation/foodloop/utils/CategoriaMapper.java
@@ -1,5 +1,25 @@
 package com.generation.foodloop.utils;
 
+import org.springframework.stereotype.Component;
+
+import com.generation.foodloop.dto.CategoriaDTO;
+import com.generation.foodloop.entities.Categoria;
+
+@Component
 public class CategoriaMapper {
-    
+    public Categoria toEntity(CategoriaDTO dto){
+        Categoria c = new Categoria();
+        updateEntity(dto,c);
+        return c;
+    }
+
+    public void updateEntity (CategoriaDTO dto, Categoria C){
+        c.setId(dto.id());
+        c.setNome(dto.nome());
+    }
+
+    public CategoriaDTO toDTO(Categoria c){
+        CategoriaDTO dto = new CategoriaDTO (c.getId(),c.getNome());
+        return dto;
+    }
 }
