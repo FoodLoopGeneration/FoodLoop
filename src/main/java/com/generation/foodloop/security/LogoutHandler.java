3db7f6f6b3bf68951b5a51decde1e51e55a1ae69
Gennaro <171047036+GenMoz22@users.noreply.github.com>
2026-02-17 10:37:24 +0100

    Aggiunti Login e Logout Handler

--- src/main/java/com/generation/foodloop/security/LogoutHandler.java
+++ src/main/java/com/generation/foodloop/security/LogoutHandler.java
@@ -1,5 +1,32 @@
 package com.generation.foodloop.security;
 
-public class LogoutHandler {
+import java.io.IOException;
 
+import org.jspecify.annotations.Nullable;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
+import org.springframework.stereotype.Component;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Component
+public class LogoutHandler implements LogoutSuccessHandler {
+    @Value("${app.cookie.nome}")
+    public String cookieName;
+
+    @Override
+    public void onLogoutSuccess(HttpServletRequest request, HttpServletResponse response,
+            @Nullable Authentication authentication) throws IOException, ServletException {
+        Cookie cookie = new Cookie(cookieName, null);
+        cookie.setHttpOnly(true);
+        cookie.setPath("/");
+        cookie.setMaxAge(0);
+        response.addCookie(cookie);
+        response.sendRedirect("/");
+
+    }
 }
