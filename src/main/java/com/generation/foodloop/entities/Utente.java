28d028afc29da42416a82daba2824405dc6a8a28
Riccardo Negri <riccardo.negri20@gmail.com>
2026-02-17 11:53:40 +0100

    Aggiunto entity Utente

--- src/main/java/com/generation/foodloop/entities/Utente.java
+++ src/main/java/com/generation/foodloop/entities/Utente.java
@@ -1,5 +1,110 @@
 package com.generation.foodloop.entities;
 
-public class Utente {
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+ @Entity
+    @Table(name = "utenti", uniqueConstraints = {
+        @UniqueConstraint(columnNames = "id"),
+        @UniqueConstraint(columnNames = "email")
+    })
+    @Data
+    @EqualsAndHashCode(onlyExplicitlyIncluded = true)
+
+public class Utente implements UserDetails{
+
+    @EqualsAndHashCode.Include
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable=false)
+    private String nome;
+
+    @Column(nullable=false)
+    private String cognome;
+
+    @EqualsAndHashCode.Include
+    @Column(nullable=false, unique = true)
+    private String email;
+
+    @Column(nullable=false)
+    private String password;
+
+    @Column(nullable=false)
+    private Set<Ingrediente> ingredienti;
+
+    @Column(nullable=false)
+    private Set<Ricetta> ricette;
+
+    private Set<Ruolo> ruoli = new HashSet<>(); 
+
+    public void aggiungiRuolo(Ruolo ruolo){
+        if(ruolo != null){
+            ruoli.add(ruolo);
+        }
+    }
+
+    public void rimuoviRuolo(Ruolo ruolo){
+        if(ruolo != null){
+            ruoli.remove(ruolo);
+        }
+    }
+
+    public void aggiungiIngrediente(Ingrediente ingrediente){
+        if(ingrediente != null){
+            ingredienti.add(ingrediente);
+        }
+    }
+
+    public void rimuoviIngrediente(Ingrediente ingrediente){
+        if(ingrediente != null){
+            ingredienti.remove(ingrediente);
+        }
+    }
+
+    public void aggiungiRicetta(Ricetta ricetta){
+        if(ricetta != null){
+            ricette.add(ricetta);
+        }
+    }
+
+    public void rimuoviRicetta(Ricetta ricetta){
+        if(ricetta != null){
+            ricette.remove(ricetta);
+        }
+    }
+    
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        List<SimpleGrantedAuthority> authorities = this.ruoli.stream()
+            .map(
+                ruolo -> new SimpleGrantedAuthority("ROLE_" + ruolo.getNome())
+            )
+            .toList();
+
+        return authorities;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.email;
+    }
     
 }
