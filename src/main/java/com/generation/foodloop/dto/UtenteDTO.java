116a6dea6b30bb50549af6ca0cd8f22c1e9409bc
Gennaro <171047036+GenMoz22@users.noreply.github.com>
2026-02-17 12:06:31 +0100

    Aggiunto UtenteDTO

--- src/main/java/com/generation/foodloop/dto/UtenteDTO.java
+++ src/main/java/com/generation/foodloop/dto/UtenteDTO.java
@@ -1,5 +1,10 @@
 package com.generation.foodloop.dto;
 
-public record UtenteDTO() {
+import java.util.Set;
+
+import com.generation.foodloop.entities.Ingrediente;
+import com.generation.foodloop.entities.Ricetta;
+
+public record UtenteDTO(Long id, Set<Ingrediente> ingredienti, Set<Ricetta> ricette, String nome, String cognome, String email, String password) {
     
 }
