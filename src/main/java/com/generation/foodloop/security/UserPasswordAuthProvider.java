b8674c3360bd5cccc3299994f17a90cbd55db51f
Riccardo Negri <riccardo.negri20@gmail.com>
2026-02-17 14:41:15 +0100

    Aggiunti EncoderContext e UserPassword

--- src/main/java/com/generation/foodloop/security/UserPasswordAuthProvider.java
+++ src/main/java/com/generation/foodloop/security/UserPasswordAuthProvider.java
@@ -1,5 +1,43 @@
 package com.generation.foodloop.security;
 
-public class UserPasswordAuthProvider {
+import org.jspecify.annotations.Nullable;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.stereotype.Component;
+
+import com.generation.foodloop.entities.Utente;
+import com.generation.foodloop.services.UtenteService;
+
+@Component
+public class UserPasswordAuthProvider implements AuthenticationProvider{
+
+    @Autowired
+    private UtenteService utenteService;
+
+    @Override
+    public @Nullable Authentication authenticate(Authentication authentication) throws AuthenticationException {
+
+        String username = authentication.getName();
+        String password = authentication.getCredentials().toString();
+        // TODO Metodo ancora da implementare da UtenteService
+        Utente utente = utenteService.findUtenteByNameAndPassword(username, password);
+        
+        if(utente != null){
+            return new UsernamePasswordAuthenticationToken(utente, null, utente.getAuthorities());
+        }
+        else{
+            throw new BadCredentialsException("Credenziali errate");
+        }
+    }
+
+    @Override
+    public boolean supports(Class<?> authentication) {
+        
+         return UsernamePasswordAuthenticationToken.class.isAssignableFrom(authentication);
+    } 
     
 }
