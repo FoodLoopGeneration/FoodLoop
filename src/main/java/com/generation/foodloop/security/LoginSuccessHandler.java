158c2087ad5008c81abec0d6055fc3303eaf29c4
Gennaro <171047036+GenMoz22@users.noreply.github.com>
2026-02-17 14:32:09 +0100

    Inizio gestione upload

--- src/main/java/com/generation/foodloop/security/LoginSuccessHandler.java
+++ src/main/java/com/generation/foodloop/security/LoginSuccessHandler.java
@@ -1,11 +1,9 @@
 package com.generation.foodloop.security;
 
 import java.io.IOException;
-import java.util.List;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 
