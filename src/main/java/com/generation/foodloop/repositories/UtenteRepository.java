ea260fc61860016ed87b534fcf631263a8dc785d
Gennaro <171047036+GenMoz22@users.noreply.github.com>
2026-02-18 10:15:05 +0100

    Aggiunta Utente Repository

--- src/main/java/com/generation/foodloop/repositories/UtenteRepository.java
+++ src/main/java/com/generation/foodloop/repositories/UtenteRepository.java
@@ -1,5 +1,15 @@
 package com.generation.foodloop.repositories;
 
-public class UtenteRepository {
-    
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.EntityGraph;
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.generation.foodloop.entities.Utente;
+
+public interface UtenteRepository extends JpaRepository<Utente, Long> {
+    @EntityGraph(attributePaths = "ruoli")
+    Optional<Utente> findWithRuoloAndUsername(String username);
+
+    Optional<Utente> findWithRuoloAndId(Long id);
+
 }
