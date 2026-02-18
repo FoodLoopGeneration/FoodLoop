189870bc74a405b7fd96ed1edba57f0c0a8e1439
Gennaro <171047036+GenMoz22@users.noreply.github.com>
2026-02-18 10:52:34 +0100

    Aggiornati Service e Repository

--- src/main/java/com/generation/foodloop/repositories/IngredienteRepository.java
+++ src/main/java/com/generation/foodloop/repositories/IngredienteRepository.java
@@ -11,5 +11,9 @@ public interface IngredienteRepository extends JpaRepository<Ingrediente, Long>{
     Optional<Ingrediente> findById(Long id);
 
     boolean existsById(Long id);
+
+    boolean existsByNome(String nome);
+    
+    boolean existsByNomeAndIdNot(String nome, Long id);
     
 }
--- src/main/java/com/generation/foodloop/repositories/RicettaRepository.java
+++ src/main/java/com/generation/foodloop/repositories/RicettaRepository.java
@@ -3,6 +3,7 @@ package com.generation.foodloop.repositories;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.generation.foodloop.entities.Ricetta;
@@ -11,11 +12,22 @@ import com.generation.foodloop.entities.Utente;
 public interface RicettaRepository extends JpaRepository<Ricetta, Long>{
     
     Optional<Ricetta> findById(Long id);
+
     Optional<Ricetta> findByNome(String nome);
+
     Optional<List<Ricetta>> findByNomeIngredienteAndUtente_id(String nome, Utente idUtente);
+
     Optional<List<Ricetta>> findByNomeIngrediente(String nome);
 
+    Optional<List<Ricetta>> findByUtenteId(Long utenteId);
+
+    @EntityGraph(attributePaths = {"ingredienti"})
+    Optional<Ricetta> findWithIngredientiById(Long id);
+
     boolean existsByNome(String nome);
-    boolean existsByNomeAndId(String nome,Long id);
+
+    boolean existsByNomeAndIdNot(String nome, Long id);
+
+    boolean existsByIdAndUtenteId(Long id, Long utenteId);
 
 }
