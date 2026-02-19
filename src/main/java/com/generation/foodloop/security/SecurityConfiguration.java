174990ec1d87c570dae4a2699950bc6b563074c0
Gennaro <171047036+GenMoz22@users.noreply.github.com>
2026-02-19 14:03:37 +0100

    Modificata Registrazione

--- src/main/java/com/generation/foodloop/security/SecurityConfiguration.java
+++ src/main/java/com/generation/foodloop/security/SecurityConfiguration.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.servlet.util.matcher.PathPatternRequestMatcher;
@@ -13,6 +14,7 @@ import com.generation.foodloop.entities.TipoRuolo;
 import com.generation.foodloop.security.filters.CookieFilter;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfiguration {
 
     @Autowired
@@ -31,15 +33,20 @@ public class SecurityConfiguration {
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
         http
-            .csrf(csrf -> csrf.disable())
+
+            .csrf(csrf -> csrf.ignoringRequestMatchers("/h2-console/**"))
+            
             .authenticationProvider(userPasswordAuthProvider)
+            
             .addFilterBefore(cookieFilter, UsernamePasswordAuthenticationFilter.class)
+            
             .authorizeHttpRequests(auth -> auth
                 .requestMatchers(ApiRoutes.PUBLIC_ENDPOINTS).permitAll()
                 .requestMatchers(ApiRoutes.USER_ENDPOINTS).hasAnyRole(TipoRuolo.USR.name(), TipoRuolo.ADM.name())
                 .requestMatchers(ApiRoutes.ADMIN_ENDPOINTS).hasRole(TipoRuolo.ADM.name())
                 .anyRequest().authenticated()
             )
+            
             .formLogin(login -> login
                 .loginPage("/login")
                 .loginProcessingUrl("/login")
@@ -47,13 +54,14 @@ public class SecurityConfiguration {
                 .failureUrl("/login?error=true")
                 .permitAll()
             )
+            
             .logout(logout -> logout
                 .logoutUrl("/logout")
                 .logoutSuccessHandler(logoutHandler)
                 .invalidateHttpSession(true)
                 .clearAuthentication(true)
-                .deleteCookies("JSESSIONID")
-                .logoutRequestMatcher(PathPatternRequestMatcher.pathPattern(HttpMethod.GET, "/logout"))
+                .deleteCookies("JSESSIONID", "FOODLOOP_AUTH")
+                .logoutRequestMatcher(PathPatternRequestMatcher.pathPattern(HttpMethod.GET,"/logout"))
                 .permitAll()
             );
 
