dfa05942b1bbdca88ac2f12ae1c629ad81627033
Gennaro <171047036+GenMoz22@users.noreply.github.com>
2026-02-19 18:16:36 +0100

    Collegato Ruolo a Utente in fase di creazione

--- src/main/java/com/generation/foodloop/services/UtenteService.java
+++ src/main/java/com/generation/foodloop/services/UtenteService.java
@@ -6,7 +6,9 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.generation.foodloop.dto.UtenteDTO;
+import com.generation.foodloop.entities.Ruolo;
 import com.generation.foodloop.entities.Utente;
+import com.generation.foodloop.repositories.RuoloRepository;
 import com.generation.foodloop.repositories.UtenteRepository;
 import com.generation.foodloop.utils.UtenteMapper;
 
@@ -20,6 +22,7 @@ public class UtenteService extends GenericService<Long, Utente, UtenteRepository
 
     private final UtenteMapper mapper;
     private final BCryptPasswordEncoder passwordEncoder;
+    private final RuoloRepository ruoloRepository;
 
     public Optional<Utente> findByEmail(String email) {
         return getRepository().findByEmail(email);
@@ -32,7 +35,7 @@ public class UtenteService extends GenericService<Long, Utente, UtenteRepository
     @Transactional
     public boolean createFromDto(UtenteDTO dto) {
         log.info("Tentativo di creazione utente per email: {}", dto.email());
-        
+
         if (getRepository().findByEmail(dto.email()).isPresent()) {
             log.warn("Registrazione fallita: email {} già presente", dto.email());
             return false;
@@ -41,7 +44,9 @@ public class UtenteService extends GenericService<Long, Utente, UtenteRepository
         try {
             Utente u = mapper.toEntity(dto);
             u.setPassword(passwordEncoder.encode(dto.password()));
-            
+            Ruolo ruoloUser = ruoloRepository.findByNome("USR")
+                    .orElseThrow(() -> new RuntimeException("Errore: Ruolo USR non trovato nel database"));
+            u.aggiungiRuolo(ruoloUser);
             Utente saved = getRepository().save(u);
             log.info("Utente registrato con successo nel DB con ID: {}", saved.getId());
             return true;
