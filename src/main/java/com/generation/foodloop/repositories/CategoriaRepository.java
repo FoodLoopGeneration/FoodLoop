97c3ea77ac6133b1ebf7bc32c7f5832b65ce0ead
Gennaro <171047036+GenMoz22@users.noreply.github.com>
2026-02-19 14:04:40 +0100

    Modificati Form Ingredienti e Ricette

--- src/main/java/com/generation/foodloop/repositories/CategoriaRepository.java
+++ src/main/java/com/generation/foodloop/repositories/CategoriaRepository.java
@@ -1,17 +1,22 @@
 package com.generation.foodloop.repositories;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.generation.foodloop.entities.Categoria;
 
-public interface CategoriaRepository extends JpaRepository<Categoria, Long>{
-    
+public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
+
     Optional<Categoria> findById(Long id);
+
     Optional<Categoria> findByNome(String nome);
 
+    List<Categoria> findByUtenteId(Long utenteId);
+
     boolean existsByNome(String nome);
-    boolean existsByNomeAndId(String nome,Long id);
+
+    boolean existsByNomeAndId(String nome, Long id);
 
 }
